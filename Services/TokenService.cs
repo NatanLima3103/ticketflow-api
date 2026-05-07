007459b95e474965b3727f912aa71d083f7562de
Natan Lima <natanjonatan3103@gmail.com>
2026-05-07 10:05:05 -0300

    feat: finaliza backend principal do TicketFlow

--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -2,6 +2,9 @@ namespace TicketFlow.API.Services
 {
     public class TokenService
     {
-        
+        public string GerarToken()
+        {
+            return "token-futuro";
+        }
     }
 }
